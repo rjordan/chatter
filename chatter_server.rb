e39dd7ba302157a7aa074a091ef9daaeb30d1561
Richard Kent Jordan <rjordan@pobox.com>
2011-11-06 13:50:43 -0600

    Added Rabbit, but behaving like direct. More work needed

--- chatter_server.rb
+++ chatter_server.rb
@@ -4,20 +4,29 @@ Bundler.require(:default)
 
 EventMachine.run {
     EventMachine::WebSocket.start(:host => "127.0.0.1", :port => 8080) do |ws|
-        ws.onopen {
-          # publish message to the queue
-          puts "{{user}} has joined the chat."
-        }
 
-        ws.onclose { 
-          # publish message to the queue
- 	  puts "{{user}} has left the chat." 
-	}
+      connection = AMQP.connect(:host => '127.0.0.1')
+      channel  = AMQP::Channel.new(connection)
+      queue = channel.queue("spikes.chatter", :auto_delete => true)
+      exchange = channel.fanout("amq.fanout")
+     
+      queue.bind(exchange).subscribe do |payload|
+        ws.send payload
+      end
+
+      ws.onopen {
+        # publish message to the queue
+        exchange.publish "{{user}} has joined the chat.", :routing_key => queue.name
+      }
+
+      ws.onclose { 
+        # publish message to the queue
+        exchange.publish "{{user}} has left the chat.", :routing_key => queue.name
+      }
         
-        ws.onmessage { |msg|
-          # publish message to the queue
-          puts "{{user}}: #{msg}"
-          ws.send "{{user}}: #{msg}"
-        }
-    end
+      ws.onmessage { |msg|
+        # publish message to the queue
+        exchange.publish "{{user}}: #{msg}", :routing_key => queue.name
+      }
+   end
 }
